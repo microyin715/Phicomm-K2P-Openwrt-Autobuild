6c80822d1f4ad8ce36cd6ad0ee631ae20c3555b1
microyin715 <66552744+microyin715@users.noreply.github.com>
2022-01-27 21:03:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,7 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 #添加额外非必须软件包
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
