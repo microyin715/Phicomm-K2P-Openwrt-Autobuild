beae4b5d676349666e05df70071238b0d3e570e9
microyin715 <66552744+microyin715@users.noreply.github.com>
2022-01-27 21:02:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.18.1/g' package/base-files/files/bin/config_generate
 
 #添加额外非必须软件包
 git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
